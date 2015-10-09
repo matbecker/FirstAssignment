85bb4d952ac4dd426e0a4eb4b7d86404fe390cbe
matbecker <7m4tt3r7@gmail.com>
2015-10-09 10:39:52 -0400

    8x8 wall
    created an 8x8 wall

---
+++ week3Unity3D/Assets/scripts/Cube.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	[SerializeField] GameObject cube;
+	[SerializeField] int x;
+	[SerializeField] int y;
+	[SerializeField] int spacing;
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.CompareTag("Bullet"))
+		{
+			Destroy(gameObject);
+		}
+	}
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < x; i++)
+		{
+			for (int j = 0; j < x; j++)
+			{
+				Vector3 pos = transform.position;
+				pos.x += i;
+				pos.y += j;
+				Instantiate(cube, pos, transform.rotation);
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			
+	}
+}
---
+++ week3Unity3D/Assets/scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Fire1"))
+		{
+			Rigidbody InstantiateProjectile = Instantiate(rb, transform.position, transform.rotation) as Rigidbody;
+			InstantiateProjectile.velocity = transform.TransformDirection(new Vector3(0f,0f,speed));
+			Debug.Log ("Firing");
+			Destroy(InstantiateProjectile.gameObject, 5f);
+		}
+
+	
+	}
+}
