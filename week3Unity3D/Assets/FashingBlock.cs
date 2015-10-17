ff67b22c71294f61d28184f560af82a14de3c21f
matbecker <7m4tt3r7@gmail.com>
2015-10-16 21:14:42 -0400

    Built up terrain. Added RAIN
    Also added some particles to the gun and made the cubes and bullets
    random colours

---
+++ week3Unity3D/Assets/FashingBlock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FashingBlock : MonoBehaviour {
+
+	[SerializeField] Material material;
+	[SerializeField] Player player;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.collider.CompareTag("Bullet"))
+		{
+			material.color = new Color(Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f),Random.Range(0.0f,1.0f));
+		}
+	}
+}
--- week3Unity3D/Assets/scripts/Player.cs
+++ week3Unity3D/Assets/scripts/Player.cs
@@ -6,21 +6,39 @@ public class Player : MonoBehaviour {
 	public float speed;
 	public Rigidbody rb;
 	[SerializeField] Transform shootPoint;
+	private float timer;
+	[SerializeField] float shotDelay;
+	private bool hasShot;
+	[SerializeField] GameObject bullet;
+	[SerializeField] Material material;
+	[SerializeField] ParticleSystem ps;
 
 	// Use this for initialization
 	void Start () {
-	
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown("z"))
+		if (hasShot)
 		{
+			ps.Play();
+			timer += Time.deltaTime;
+		}
+		if (timer > shotDelay)
+		{
+			timer = 0.0f;
+			hasShot = false;
+		}
+
+		if (Input.GetKey("z") && timer == 0.0f)
+		{
+			hasShot = true;
 			Rigidbody InstantiateProjectile = Instantiate(rb, shootPoint.position, transform.rotation) as Rigidbody;
-			InstantiateProjectile.velocity = transform.TransformDirection(new Vector3(0f,0f,speed));
-			Debug.Log ("Firing");
-			Destroy(InstantiateProjectile.gameObject, 5f);
+			InstantiateProjectile.velocity = transform.TransformDirection(new Vector3(0.0f,0.0f,speed));
+			Destroy(InstantiateProjectile.gameObject, 1f);
 		}
 
 	
